d69345cc94592789f4aa008f3b432c7a3f6ee9cd
DarkenLM <rafaelsantosfernandes660@gmail.com>
2023-04-26 12:35:15 +0100

    feat(util): added ncurses util
    - Added ncurses util. Added rectangle function.

---
+++ include/util/ncurses.h
@@ -0,0 +1,17 @@
+#ifndef __RL_UTIL_NCURSES_H
+#define __RL_UTIL_NCURSES_H
+
+#include <ncurses.h>
+
+inline void rectangle(WINDOW* win, int y1, int x1, int y2, int x2) {
+    mvwhline(win, y1, x1, 0, x2-x1);
+    mvwhline(win, y2, x1, 0, x2-x1);
+    mvwvline(win, y1, x1, 0, y2-y1);
+    mvwvline(win, y1, x2, 0, y2-y1);
+    mvwaddch(win, y1, x1, ACS_ULCORNER);
+    mvwaddch(win, y2, x1, ACS_LLCORNER);
+    mvwaddch(win, y1, x2, ACS_URCORNER);
+    mvwaddch(win, y2, x2, ACS_LRCORNER);
+}
+
+#endif

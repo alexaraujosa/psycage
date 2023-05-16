089904444e3a447369bd1f3aa51f61c830957b64
alexaraujosa <a104257@alunos.uminho.pt>
2023-05-16 22:06:21 +0100

    refactor(interface): adicionados defines para a Interface
    == INTERFACE
    
    - Adicionados defines para facilitar troca da Largura e Altura do Jogo.

--- src/gameloop/gameloop.c
+++ src/gameloop/gameloop.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include "gameloop.h"
 
+#define ALTURA_JOGO g_renderstate->nrows - ALTURA_LOGO - 2
+#define LARGURA_JOGO g_renderstate->ncols - 2
+
 Gamestate g_gamestate;
 int** map_footprint;
 int find_map;
@@ -36,19 +39,19 @@ Gamestate init_gameloop() {
 	// gs->path_cells = NULL;
 
     debug_file(dbgOut, " - Generating initial map...\n");
-	find_map = create_random_map(g_renderstate->nrows - ALTURA_LOGO - 2, g_renderstate->ncols - 2, ALTURA_LOGO + 1, 1); // map
+	find_map = create_random_map(ALTURA_JOGO, LARGURA_JOGO, ALTURA_LOGO + 1, 1); // map
 
     debug_file(dbgOut, " -- Generating light map...\n");
 	init_light_map(g_renderstate->nrows, g_renderstate->ncols);
 
 
     debug_file(dbgOut, " - Generating map footprint...\n");
-	map_footprint = (int **)malloc((g_renderstate->nrows - ALTURA_LOGO - 2) * sizeof(int *));
-    for (int i = 0; i < g_renderstate->nrows - ALTURA_LOGO - 2; i++) {
+	map_footprint = (int **)malloc((ALTURA_JOGO) * sizeof(int *));
+    for (int i = 0; i < ALTURA_JOGO; i++) {
         map_footprint[i] = (int *)malloc(g_renderstate->ncols * sizeof(int));
     }
 
-	for (int i = 0; i < g_renderstate->nrows - ALTURA_LOGO - 2; i++) {
+	for (int i = 0; i < ALTURA_JOGO; i++) {
 		for (int j = 0; j < g_renderstate->ncols; j++) {
 			map_footprint[i][j] = map[i][j];
 		}
@@ -63,7 +66,7 @@ Gamestate init_gameloop() {
 	for (int i = 0; i < mob_count; i++) {
 		Mob mob = defaultMob();
 
-		addMobToMap(mob, map, g_renderstate->ncols, g_renderstate->nrows - ALTURA_LOGO - 2);
+		addMobToMap(mob, map, g_renderstate->ncols, ALTURA_JOGO);
 
 		mobs[i] = mob;
 		map[mob->entity->coords->y][mob->entity->coords->x] = 5;

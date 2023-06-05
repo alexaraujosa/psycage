69e5899ce423f25d5590e49fb6a096edb2590a74
alexaraujosa <a104257@alunos.uminho.pt>
2023-06-05 19:24:13 +0100

    feat(potions): adicionado codigo-fonte relativo ao sistema de poções

---
+++ src/data/potions.c
@@ -0,0 +1,66 @@
+#include "potions.h"
+
+#define TEMPO_POTION_STRENGTH 20
+
+Clock clock_strength = NULL;
+
+void init_potions_clock() {
+    clock_strength = defaultClock();
+    clock_strength->maxTicks *= TEMPO_POTION_STRENGTH;
+    clock_strength->blocked = 1;
+    addClock(clock_strength);
+
+    return;
+}
+
+
+void create_potion() {
+
+    int cellX = -1, cellY = -1;
+    
+    while ((cellX == -1 && cellY == -1) || _isObstacle(map[cellY][cellX])) {
+        cellX = (int)floor(((double) rand() / RAND_MAX) * (LARGURA_JOGO - 1));
+        cellY = (int)floor(((double) rand() / RAND_MAX) * (ALTURA_JOGO - 1));
+    }
+
+    map[cellY][cellX] = 7;
+
+    return;
+}
+
+
+void use_random_potion() {
+
+    int temporary = (rand() % 2);
+
+    if(temporary) {
+        g_gamestate->player->entity->basedamage *= 2;
+        g_gamestate->potion_strength = 1;
+        clock_strength->blocked = 0;
+    }
+    else
+        healEntity(g_gamestate->player->entity, g_gamestate->player->entity->maxHealth/3);
+    
+    map[g_gamestate->player->entity->coords->y][g_gamestate->player->entity->coords->x] = 0;
+
+    return;
+}
+
+
+void remove_potion_strength_effect() {
+
+    g_gamestate->player->entity->basedamage /= 2;
+    g_gamestate->potion_strength = 0;
+    clock_strength->blocked = 1;
+
+    return;
+}
+
+
+void potion_checker() {
+
+    if(clock_strength->ticks == TICKS_PER_SECOND*TEMPO_POTION_STRENGTH - 1)
+        remove_potion_strength_effect();
+    
+    return;
+}
---
+++ src/data/potions.h
@@ -0,0 +1,15 @@
+#ifndef __RL_DATA_POTIONS_H
+#define __RL_DATA_POTIONS_H
+
+#include "common.h"
+#include "../entity/ai/ai.h"
+#include "../gameloop/gameloop.h"
+
+
+/* FUNCTION SIGNATURES */
+void create_potion();
+void use_random_potion();
+void remove_potion_strength_effect();
+void potion_checker();
+
+#endif
